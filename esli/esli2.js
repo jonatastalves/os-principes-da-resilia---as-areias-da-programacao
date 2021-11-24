9262e04cc44a213f57d029b54aae02bb3386378c
jonatastalves <jonatas.t.alves@gmail.com>
2021-11-23 21:17:36 -0300

    Adição de funcoes e paginas ao projeto

---
+++ esli/esli2.js
@@ -0,0 +1,30 @@
+principeEsli ();
+
+function principeEsli (){
+
+    while(true){
+
+    var escolha = prompt ("1 - lorem ipsum dolor \n 2 - lorem ipsum dolor erro");
+
+    if (escolha == 2){
+
+        document.getElementById("resposta-js").innerHTML = (`
+        
+        <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium modi,<br> autem ea labore quos saepe totam suscipit qui numquam sint provident temporibus <br>nihil quis placeat sunt quaerat quibusdam odio est.<br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium modi,<br> autem ea labore quos saepe totam suscipit qui numquam sint provident temporibus <br>nihil quis placeat sunt quaerat quibusdam odio est.<br> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium modi,<br> autem ea labore quos saepe totam suscipit qui numquam sint provident temporibus <br>nihil quis placeat sunt quaerat quibusdam odio est.<br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium modi,<br> autem ea labore quos saepe totam suscipit qui numquam sint provident temporibus <br>nihil quis placeat sunt quaerat quibusdam odio est.</h1><br><br><br>
+        <a href="../esli/esli3.html"><button class="play-button">RESPONDER</button></a>`);
+        break;
+    }
+
+    else if (escolha == 1){
+        document.getElementById("resposta-js").innerHTML = (`
+        
+        <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium modi,<br> autem ea labore quos saepe totam suscipit qui numquam sint provident temporibus <br>nihil quis placeat sunt quaerat quibusdam odio est.<br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium modi,<br> autem ea labore quos saepe totam suscipit qui numquam sint provident temporibus <br>nihil quis placeat sunt quaerat quibusdam odio est.<br> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium modi,<br> autem ea labore quos saepe totam suscipit qui numquam sint provident temporibus <br>nihil quis placeat sunt quaerat quibusdam odio est.<br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium modi,<br> autem ea labore quos saepe totam suscipit qui numquam sint provident temporibus <br>nihil quis placeat sunt quaerat quibusdam odio est.</h1><br><br><br>
+        <a href="../index.html"><button class="play-button">REINICIAR</button></a>`);
+        break;
+    }
+    else{     
+        alert ("NÚMERO INVÁLIDO, ESCOLHA NOVAMENTE!");   
+    }
+
+    }
+}
---
+++ raiz.js
@@ -0,0 +1,8 @@
+var nomeJogador = prompt("Bem vindo! Digite seu nome.");
+
+document.getElementById("nome-jogador").innerHTML = (nomeJogador);
+
+function scrollWin() {
+    window.scrollTo(0, 1000);
+  }
+
